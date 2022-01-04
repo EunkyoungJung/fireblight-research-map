6b00a7120fcaaf31504d5d603ba4f4e35bc9637c
EunkyoungJung <muchogusto.lol@gmail.com>
2022-01-04 13:22:08 +0900

    Updates

---
+++ precache-manifest.54316e8acfe5bd7722d72ad0cddb3ee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9bfcbb90c8455586988a928cc2a98c",
+    "url": "/fireblight-research-map/index.html"
+  },
+  {
+    "revision": "35cb62350ffadefe1695",
+    "url": "/fireblight-research-map/static/css/main.b32b557d.chunk.css"
+  },
+  {
+    "revision": "b6a8bdc0a9060c8aaf16",
+    "url": "/fireblight-research-map/static/js/2.b84f0967.chunk.js"
+  },
+  {
+    "revision": "97af2597212b028677ebd4da1feac364",
+    "url": "/fireblight-research-map/static/js/2.b84f0967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cb62350ffadefe1695",
+    "url": "/fireblight-research-map/static/js/main.687e54c3.chunk.js"
+  },
+  {
+    "revision": "cc3aeefc09df8a17fa86",
+    "url": "/fireblight-research-map/static/js/runtime-main.ef799fb1.js"
+  },
+  {
+    "revision": "ee2fda570eed27063366cd38481de6f7",
+    "url": "/fireblight-research-map/static/media/fireblight_logo.ee2fda57.png"
+  }
+]);
